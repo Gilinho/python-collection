b9cf9a2597fa627261d2adf7635385ac376bb1c1
Yodi aditya <bashlook@gmail.com>
2012-07-23 22:26:51 +0800

    modify timer example

--- basic/benchmark-timer.py
+++ basic/benchmark-timer.py
@@ -1,8 +1,9 @@
 from timeit import Timer
 
+
 def calculate(x, y):
     return x * y
 
 if __name__ == '__main__':
-    t = Timer("calculate(100009, 1000000)", "from __main__ import  calculate")
+    t = Timer("calculate(2, 34)", "from __main__ import  calculate")
     print t.timeit()
